6d55a9820c64cd12dcf9631c6330f833da53402a
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-03-10 22:02:26 +0530

    Externalized | API_KEY

--- deploy-apps.sh
+++ deploy-apps.sh
@@ -1,2 +1,4 @@
-docker run -d --name covidstat-mvp-be --network=casestudy -p 8080:8080 covidstat:0.0.1
-docker run -d --name covidstat-mvp-fe --network=casestudy -p 3000:80 covistat-fe:0.0.1
+API_KEY=$1
+
+docker run -d --name covidstat-mvp-be -e API_KEY=$API_KEY --network=casestudy -p 8080:8080 covidstat:0.0.1
+docker run -d --name covidstat-mvp-fe  --network=casestudy -p 3000:80 covistat-fe:0.0.1
